89f129a3cdb420353026021ad631e326e89f0273
MartinMekk <martin.solheim@me.com>
2014-12-06 15:15:19 +0100

    New exercise answered

--- sixth_of_december.py
+++ sixth_of_december.py
@@ -1,1 +1,10 @@
 __author__ = 'martinsolheim'
+
+all_products = []
+
+for i in range(1, 8001):
+    for j in range(i, 8001):
+        all_products.append(i * j)
+    print(8000 - i, 'runs left')
+
+print(len(set(all_products)))
