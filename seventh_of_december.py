dca61e9dd12d563c94a82e5cd188b41eeee359be
MartinMekk <martin.solheim@me.com>
2014-12-07 14:13:25 +0100

    new image added, problem nr. 7 solved

--- seventh_of_december.py
+++ seventh_of_december.py
@@ -1,1 +1,28 @@
 __author__ = 'martinsolheim'
+
+
+from PIL import Image
+from collections import defaultdict
+import os
+
+path_dir = os.path.dirname(__file__)
+filename = os.path.join(path_dir, "data/santa.png")
+
+im = Image.open(filename)
+pixels = im.load()
+
+width, height = im.size
+
+pixel_dict = defaultdict(int)
+for x in range(width):
+    for y in range(height):
+        pixel = pixels[x, y]
+        pixel_dict[pixel] += 1
+
+pixel_values = []
+for value in pixel_dict.values():
+    pixel_values.append(value)
+
+pixel_values.sort(reverse=True)
+
+print(pixel_values[9])
