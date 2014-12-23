b69d2b87cb7a43e856aa7d243aae22633d301ae1
maria <maremoy@live.no>
2014-12-23 15:17:50 +0100

    Selved task 23

---
+++ twentythird_of_december_mar.py
@@ -0,0 +1,19 @@
+__author__ = 'maria'
+
+
+def is_number_correct(number):
+    first_number = ""
+    number_list = map(str, str(number))
+    for i in range(0, len(number_list)-1):
+        first_number += number_list[i]
+        second_number = ''.join(number_list[i+1:len(number_list)])
+        if (int(first_number) + int(second_number))**2 == number:
+            return True
+    return False
+
+count = 0
+for n in range(10, 1000000):
+    if is_number_correct(n):
+        count += 1
+
+print count
